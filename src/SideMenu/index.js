47bad58f7f007a931537d76fe0366f2bd57db6e4
n313893254 <n313893254@126.com>
2018-01-03 15:50:56 +0800

    fix

--- src/SideMenu/index.js
+++ src/SideMenu/index.js
@@ -19,17 +19,20 @@ export default class C extends React.Component {
           >
             <Avatar size="large"
                     className="logo mt-20 ml-10"
-                    style={{width: '150px', height: '150px', 'border-radius': '50%'}}
+                    style={{width: '150px', height: '150px', 'borderRadius': '50%'}}
                     src="https://avatars2.githubusercontent.com/u/18737885?s=400&u=51c729c1f22c4846e7e75401af72fe9cab506510&v=4"
             />
           </MenuItem>
-          <MenuItem
+          <MenuItem className="mt-0 mb-0"
           >
-            {/* <div className={styles.mask}>hello</div> */}
             <span>
-              <span>
-                博客
-              </span>
+              博客
+            </span>
+          </MenuItem>
+          <MenuItem className="mt-0 mb-0"
+          >
+            <span>
+              ATD
             </span>
           </MenuItem>
         </Menu>
